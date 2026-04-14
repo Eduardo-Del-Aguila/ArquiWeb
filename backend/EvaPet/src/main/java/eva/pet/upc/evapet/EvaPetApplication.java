144f6e31695dd8f547d7f88d66baf6d909830c2a
TORTUGAPLATEADA <eduardodelaguila10@gmail.com>
2026-04-14 00:04:51 -0500

    backend created

---
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/EvaPetApplication.java
@@ -0,0 +1,16 @@
+package eva.pet.upc.evapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaPetApplication.class, args);
+    }
+
+}
+
+
+
