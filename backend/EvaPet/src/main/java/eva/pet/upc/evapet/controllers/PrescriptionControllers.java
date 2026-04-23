fcfd8b9480a1624605523526ab189337bf1a61e6
Rosy Mayta <mayta.rosy122@gmail.com>
2026-04-23 16:15:38 -0500

    feat: add Prescription CRUD endpoints and DTOs

---
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/controllers/PrescriptionControllers.java
@@ -0,0 +1,102 @@
+package eva.pet.upc.evapet.controllers;
+
+import eva.pet.upc.evapet.dtos.PrescriptionDTO;
+import eva.pet.upc.evapet.dtos.PrescriptionInsertDTO;
+import eva.pet.upc.evapet.models.Prescription;
+import eva.pet.upc.evapet.serviceInterfaces.IPrescriptionService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/prescriptions")
+public class PrescriptionControllers {
+    @Autowired
+    private IPrescriptionService pS;
+
+    @GetMapping
+    public ResponseEntity<List<PrescriptionDTO>> listar(){
+        ModelMapper m = new ModelMapper();
+
+        List<PrescriptionDTO> lista = pS.list().stream()
+                .map(y -> m.map(y, PrescriptionDTO.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registrar(@RequestBody PrescriptionInsertDTO dto){
+
+        if(dto.getDiagnosis() == null || dto.getDiagnosis().isEmpty()){
+            return ResponseEntity.badRequest().body("El diagnóstico no puede ser vacío");
+        }
+
+        ModelMapper m = new ModelMapper();
+        Prescription p = m.map(dto, Prescription.class);
+
+        Prescription nuevo = pS.insert(p);
+
+        PrescriptionInsertDTO responseDTO = m.map(nuevo, PrescriptionInsertDTO.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable int id){
+
+        ModelMapper m = new ModelMapper();
+        Optional<Prescription> p = pS.listId(id);
+
+        if(p.isPresent()){
+            PrescriptionInsertDTO dto = m.map(p.get(), PrescriptionInsertDTO.class);
+            return ResponseEntity.ok(dto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Prescripcion no encontrado");
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<String> actualizar(@RequestBody PrescriptionInsertDTO dto){
+
+        Optional<Prescription> existente = pS.listId(dto.getIdPrescription());
+
+        if(existente.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Prescripcion no encontrado");
+        }
+
+        Prescription p = existente.get();
+
+        p.setIdUserPatient(dto.getIdUserPatient());
+        p.setIdEva(dto.getIdEva());
+        p.setDiagnosis(dto.getDiagnosis());
+        p.setDate(dto.getDate());
+
+        pS.update(p);
+
+        return ResponseEntity.ok("Prescripcion actualizado correctamente");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable int id){
+
+        Optional<Prescription> p = pS.listId(id);
+
+        if(p.isPresent()){
+            pS.delete(id);
+            return ResponseEntity.ok("Prescripcion eliminado correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Prescription no encontrado");
+        }
+    }
+
+}
---
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/dtos/PrescriptionDTO.java
@@ -0,0 +1,42 @@
+package eva.pet.upc.evapet.dtos;
+
+import java.time.LocalDate;
+
+public class PrescriptionDTO {
+    private int idUserPatient;
+    private int idEva;
+    private String diagnosis;
+    private LocalDate date;
+
+    public int getIdUserPatient() {
+        return idUserPatient;
+    }
+
+    public void setIdUserPatient(int idUserPatient) {
+        this.idUserPatient = idUserPatient;
+    }
+
+    public int getIdEva() {
+        return idEva;
+    }
+
+    public void setIdEva(int idEva) {
+        this.idEva = idEva;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
