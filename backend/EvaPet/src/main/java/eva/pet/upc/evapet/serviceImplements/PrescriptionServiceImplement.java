1d19b547a5e03f6f10748b353befe2c100112d2b
Rosy Mayta <mayta.rosy122@gmail.com>
2026-04-23 16:15:16 -0500

    feat: implement Prescription service layer

---
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/serviceImplements/PrescriptionServiceImplement.java
@@ -0,0 +1,44 @@
+package eva.pet.upc.evapet.serviceImplements;
+
+import eva.pet.upc.evapet.models.Prescription;
+import eva.pet.upc.evapet.repositories.IMedicationsRepository;
+import eva.pet.upc.evapet.repositories.IPrescriptionRespository;
+import eva.pet.upc.evapet.serviceInterfaces.IPrescriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PrescriptionServiceImplement implements IPrescriptionService {
+
+    @Autowired
+    private IPrescriptionRespository pRepo;
+
+
+    @Override
+    public List<Prescription> list() {
+        return pRepo.findAll();
+    }
+
+    @Override
+    public Prescription insert(Prescription pre) {
+        return pRepo.save(pre);
+    }
+
+    @Override
+    public Optional<Prescription> listId(int id) {
+        return pRepo.findById(id);
+    }
+
+    @Override
+    public void update(Prescription p) {
+        pRepo.save(p);
+    }
+
+    @Override
+    public void delete(int id) {
+        pRepo.deleteById(id);
+    }
+}
---
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/serviceInterfaces/IPrescriptionService.java
@@ -0,0 +1,19 @@
+package eva.pet.upc.evapet.serviceInterfaces;
+
+import eva.pet.upc.evapet.models.Prescription;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface IPrescriptionService {
+    public List<Prescription> list();
+
+    Prescription insert(Prescription pre);
+
+    Optional<Prescription> listId(int id);
+
+    void update(Prescription p);
+
+    void delete(int id);
+}
