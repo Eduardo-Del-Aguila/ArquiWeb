2f0677e849f09345c18f83315e8ff9e69b3dfd2e
Rosy Mayta <mayta.rosy122@gmail.com>
2026-04-23 14:49:09 -0500

    feat: implement Medications service CRUD

--- backend/EvaPet/src/main/java/eva/pet/upc/evapet/serviceImplements/MedicationsServiceImplement.java
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/serviceImplements/MedicationsServiceImplement.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedicationsServiceImplement implements IMedicationsService {
@@ -17,5 +18,26 @@ public class MedicationsServiceImplement implements IMedicationsService {
     public List<Medications> list()
     {
         return mRepo.findAll();
+    }
+
+    @Override
+    public Medications insert(Medications med) {
+        return mRepo.save(med);
+    }
+
+    @Override
+    public Optional<Medications> listId(int id) {
+        return mRepo.findById(id);
+    }
+
+    @Override
+    public void update(Medications m) {
+        mRepo.save(m);
+    }
+
+    @Override
+    public void delete(int id) {
+        mRepo.deleteById(id);
+
     }
 }
