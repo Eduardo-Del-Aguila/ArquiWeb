0d826217d3fef51e6669d14d1f4fbde31a23fd43
Nayara Maldonado Ayala <nayaramaldonadoayala@MacBook-Air-de-Nayara.local>
2026-05-09 19:14:43 -0500

    Pull origin nayara

--- backend/EvaPet/src/main/java/eva/pet/upc/evapet/controllers/SymptomController.java
+++ backend/EvaPet/src/main/java/eva/pet/upc/evapet/controllers/SymptomController.java
@@ -7,12 +7,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+//@PreAuthorize("hasAuthority" )
 @RestController
 public class SymptomController {
     @Autowired
